79e191860db4f60d56cf72b88a4ffb2f1b873dfd
IoNuT <ionut_cristian_boss@yahoo.it>
2023-03-20 15:14:42 +0200

    Update super.php

--- src/Commands/super.php
+++ src/Commands/super.php
@@ -12,7 +12,7 @@ $super = function (int $who, array $message, int $type) {
     if (empty($message[1])) {
         return $bot->network->sendMessageAutoDetection(
             $who,
-            'Usage !super [anime/summer/halloween/xmas/heart/hobbies] [user]',
+            'Usage !super [anime/summer/halloween/xmas/heart/hobbies/cary/santa/love/egg/kao/fx/bunny/bf/zoo] [user]',
             $type
         );
     }
@@ -115,6 +115,145 @@ $super = function (int $who, array $message, int $type) {
             'swimming' => 550,
             'acting' => 557,
         ];
+
+        $cary = [
+            'spooky'	 => 148,
+            'vampyre'	 => 202,
+            'zombie' 	=> 213,
+            'scary' 	=> 254,
+            'zwhack'	=> 256,
+			'arachnid'	=> 270,
+			'reaper'	=> 354,
+			'boo'		=> 411,
+			'graveyard'	=> 412,
+			'ghostmon'	=> 446,
+			'nightmare'	=> 454,
+			'skull'		=> 464,
+			'fear'		=> 565,
+			'spookies'	=> 567,
+		];
+
+		$santa = [
+            'snowman'		=> 154,
+            'reindeer'		=> 155,
+            'santa'			=> 156,
+            'claus'			=> 204,
+            'noel'			=> 262,
+			'toys'			=> 263,
+			'reveal'		=> 314,
+			'holidays'		=> 315,
+			'sleighhug'		=> 367,
+			'xmasfactory'	=> 475,
+			'kris'			=> 571,
+			'neva'			=> 573,
+		];
+
+		$love = [
+			'valentine'		=> 62,
+			'marriage'		=> 241,
+			'wedding'		=> 233,
+			'romance'		=> 242,
+			'floral'		=> 335,
+			'lovemix'		=> 374,
+			'lovemix2'		=> 425,
+			'lovefx'		=> 426,
+			'lovetest'		=> 427,
+			'heartbreak'	=> 482,
+			'lovepotion'	=> 583,
+		];
+
+		$egg = [
+			'eggs'			=> 222,
+			'eggy'			=> 251,
+			'easteregg'		=> 281,
+			'easterlove'	=> 332,
+			'easterland'	=> 381,
+			'eggie'			=> 440,
+		];
+
+		$kao = [ 
+			'gkaoani'	=> 72,
+			'gkaliens'	=> 76,
+			'kangel'	=> 248,
+			'kdemon'	=> 249,
+			'kmoon'		=> 305,
+			'ksun'		=> 306,
+			'kandle'	=> 358,
+			'kstar'		=> 361,
+			'ricebowl'	=> 372,
+			'onion'		=> 385,
+			'kaoears'	=> 422,
+			'kloud'		=> 451,
+			'kveggie'	=> 456,
+			'koffee'	=> 499,
+			'ktree'		=> 507,
+			'kcar'		=> 520,
+			'khair'		=> 529,
+			'kactus'	=> 527,
+			'kbacks'	=> 542,
+		];
+
+		$fx = [
+			'magicfx'		=> 163,
+			'pulsefx'		=> 174,
+			'spiralfx'		=> 178,
+			'vortexfx'		=> 182,
+			'whirlfx'		=> 187,
+			'clockfx'		=> 198,
+			'glitterfx'		=> 208,
+			'phasefx'		=> 240,
+			'bitefx'		=> 275,
+			'gamefx'		=> 289,
+			'ballfx'		=> 293,
+			'gamefx2'		=> 313,
+			'spacefx'		=> 432,
+			'firefx'		=> 459,
+			'illusionfx'	=> 484,
+			'portalfx'		=> 501,
+			'slimefx'		=> 508,
+			'energyfx'		=> 569,
+			'lavafx'		=> 621,
+			'circlefx'		=> 631,
+		];
+
+		$bunny = [
+			'easter'		=> 68,
+			'blubunni'		=> 300,
+			'ebunny'		=> 380,
+			'ribunny'		=> 436,
+			'rabb'			=> 605,
+			'littlebunny'	=> 645,
+		];
+
+		$bf = [
+			'blackfriday'	=> 260,
+			'money'			=> 509,
+			'blacksale'		=> 572,
+			'darkfriday'	=> 612,
+			'trader'		=> 613,
+			'fridaysales'	=> 656,
+		];
+
+		$zoo = [
+			'pony'		=> 197,
+			'kmonkey'	=> 216,
+			'arachnid'	=> 270,
+			'ceebear'	=> 341,
+			'lions'		=> 352,
+			'tigers'	=> 364,
+			'koala'		=> 389,
+			'elephant'	=> 405,
+			'kgiraffe'	=> 428,
+			'hedgehog'	=> 438,
+			'hippos'	=> 489,
+			'guineapig'	=> 495,
+			'raccoons'	=> 504,
+			'gorilla'	=> 516,
+			'skunk'		=> 537,
+			'meerkat'	=> 539,
+			'king'		=> 570,
+			'scamp'		=> 580,
+		];
 
         $powersToCheck = ${$message[1]};
 
